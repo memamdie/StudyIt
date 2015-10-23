ca14c755acdc019888db7c2f27b18dcf8ddbe8b1
memamdie <memamdie@ufl.edu>
2015-10-23 10:18:12 -0400

    linked to parse project

--- BigProject/AppDelegate.swift
+++ BigProject/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("TfSkmY7xpeaEoOYlZDpEdGRUpetGmMqWNkaoKSQJ",
+            clientKey: "hKQjNAYNN9BkmCfZytSe5Kyrrd4ewt1hduNeQ6N6")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
         // Override point for customization after application launch.
         return true
     }
